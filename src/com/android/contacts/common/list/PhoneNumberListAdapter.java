b4dae55971193fa65483d22de13b30e8e50b2e6b
martincz <martincz@mokeedev.com>
2016-10-11 19:02:47 +0800

    ContactsCommon: display phone location in contact
    Change-Id: I0469acac064c24ea232a4409edb8797c9491d700

--- src/com/android/contacts/common/list/PhoneNumberListAdapter.java
+++ src/com/android/contacts/common/list/PhoneNumberListAdapter.java
@@ -50,6 +50,7 @@ import com.android.contacts.common.preference.ContactsPreferences;
 import com.android.contacts.common.util.Constants;
 
 import com.google.common.collect.Lists;
+import com.mokee.cloud.location.OfflineNumber;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -486,6 +487,8 @@ public class PhoneNumberListAdapter extends ContactEntryListAdapter {
         final String text;
         if (displayNumber) {
             text = cursor.getString(PhoneQuery.PHONE_NUMBER);
+            label = OfflineNumber.detect(text, getContext());
+            view.setLabel(label);
         } else {
             // Display phone label. If that's null, display geocoded location for the number
             final String phoneLabel = cursor.getString(PhoneQuery.PHONE_LABEL);
